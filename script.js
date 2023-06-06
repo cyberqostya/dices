ba0dc656741001af517fe2185910c95710126da6
k.mikov <1konstantinmikov@gmail.com>
2023-06-06 14:44:57 +0300

    06.06

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 import { styles } from './style.js';
 
 // Установка высоты вьюпорта для apple
-document.documentElement.style.setProperty(
-  "--body-height",
-  `${window.innerHeight}px`
-);
+document.documentElement.style.setProperty( "--body-height", `${window.innerHeight}px`);
 // Добавление цвета кубикам
 const randomStyle = Math.floor(Math.random() * styles.length);
 document.body.style.setProperty('--background', styles[randomStyle][0]);
 document.body.style.setProperty('--borderdot', styles[randomStyle][1]);
+// Добавление размера кубикам
+document.body.style.setProperty('--container-scale', 2);
 
 
 
@@ -39,8 +38,6 @@ const laugh = new Audio('./sounds/laugh.mp3');
 const devil = document.querySelector('.devil');
 const fire = document.querySelector('.fire');
 
-let rollCubes = [];
-let rollResult = [];
 
 // Таймер неактивности интефейса при бросках
 let inactivityTimeout;
@@ -51,6 +48,15 @@ const infoButton = document.querySelector(".info");
 const infoOverlay = document.querySelector(".info__overlay");
 
 
+// Изменить размер кубикам
+function changeCubesSize() {
+  switch(cubesContainer.children.length) {
+    case 1: document.body.style.setProperty('--container-scale', 2); break;
+    case 2: document.body.style.setProperty('--container-scale', 1.7); break;
+    case 3: document.body.style.setProperty('--container-scale', 1.2); break;
+    case 4: document.body.style.setProperty('--container-scale', 1); break;
+  }
+}
 // Активировать интерфейс
 function activeButtons() {
   rollButton.classList.remove('_disabled');
@@ -89,11 +95,11 @@ function roll(cube) {
   inactivityTimeout = setTimeout(activeButtons, 6000);
 
   console.log(result, failCounter, successCounter);
-  rollResult.push(result);
+  return result;
 }
 
-function checkResults() {
-  if (rollResult.some(i => i >= 5)) {
+function checkResults(cubes, results) {
+  if (results.some(i => i >= 5)) {
     failCounter = 0;
     successCounter++;
   } else {
@@ -108,7 +114,7 @@ function checkResults() {
       // Для обхода воспроизведения на iphone
       harp.play(); harp.pause();
       setTimeout(luckCome, 6500);
-      setTimeout(() => { reverseCubeByExternalForces('luck') }, 7500);
+      setTimeout(() => { reverseCubeByExternalForces('luck', cubes, results) }, 7500);
 
       failCounter = 0;
     }
@@ -121,7 +127,7 @@ function checkResults() {
       // Для обхода воспроизведения на iphone
       laugh.play(); laugh.pause();
       setTimeout(devilCome, 6500);
-      setTimeout(() => { reverseCubeByExternalForces('devil') }, 7500);
+      setTimeout(() => { reverseCubeByExternalForces('devil', cubes, results) }, 7500);
 
       successCounter = 0;
     }
@@ -157,7 +163,7 @@ function getNearestAngleSuccess(styleTransform, value) {
   return `rotateX(${resultsCoords[value - 1][0] + 360 * quantityXRotations}deg) rotateY(${resultsCoords[value - 1][1] + 360 * quantityYRotations}deg)`;
 }
 // Поворот кубов высшими силами
-function reverseCubeByExternalForces(force) {
+function reverseCubeByExternalForces(force, cubes, results) {
   const luckCubesQuantity = Math.random() > .7 ? (Math.random() > .8 ? (Math.random() > .9 ? (Math.random() > .95 ? 5 : 4) : 3) : 2) : 1;
   const devilCubesQuantity = Math.random() > .6 ? (Math.random() > .7 ? (Math.random() > .8 ? (Math.random() > .9 ? 5 : 4) : 3) : 2) : 1;
 
@@ -171,18 +177,18 @@ function reverseCubeByExternalForces(force) {
       break;
   }
 
-  for (let i = 0; (i < cubesQuantity && i < rollCubes.length); i++) {
+  for (let i = 0; (i < cubesQuantity && i < cubes.length); i++) {
 
     let targetCubeIndex;
     switch (force) {
       case 'luck':
-        targetCubeIndex = rollResult.indexOf(rollResult.find(i => i === 1 || i === 2 || i === 3 || i === 4));
+        targetCubeIndex = results.indexOf(results.find(i => i === 1 || i === 2 || i === 3 || i === 4));
         break;
       case 'devil':
-        targetCubeIndex = rollResult.indexOf(rollResult.find(i => i === 6 || i === 5));
+        targetCubeIndex = results.indexOf(results.find(i => i === 6 || i === 5));
         break;
     }
-    const targetCube = rollCubes[targetCubeIndex];
+    const targetCube = cubes[targetCubeIndex];
 
     targetCube.style.transitionDuration = '2000ms';
     
@@ -197,8 +203,8 @@ function reverseCubeByExternalForces(force) {
     }
 
     targetCube.style.transform = getNearestAngleSuccess(targetCube.style.transform, value);
-    rollResult[targetCubeIndex] = value;
-    console.log(rollResult);
+    results[targetCubeIndex] = value;
+    console.log(results);
 
     setTimeout(() => { targetCube.style.transitionDuration = '6000ms'; }, 2000);
   }
@@ -209,27 +215,25 @@ function reverseCubeByExternalForces(force) {
 // Добавление и удаление кубика
 addButton.addEventListener("click", () => {
   cubesContainer.insertAdjacentElement("beforeend", cubeHTML.cloneNode(true));
+  changeCubesSize();
 });
 removeButton.addEventListener("click", () => {
   cubesContainer.children[0].remove();
+  changeCubesSize();
 });
 // Ролл одного
 cubesContainer.addEventListener("click", (e) => {
   if (e.target.closest(".cube")) {
-    rollCubes = [e.target.closest(".cube")];
-    rollResult = [];
-
-    roll(rollCubes[0]);
-    checkResults();
+    const cube = e.target.closest(".cube");
+    const rollResult = roll(cube);
+    checkResults([cube], [rollResult]);
   }
 });
 // Ролл общий
 rollButton.addEventListener("click", () => {
-  rollCubes = Array.from(cubesContainer.children).sort((a, b) => (Math.random() > 0.5 ? 1 : -1)).map(i => i.querySelector(".cube"));
-  rollResult = [];
-  
-  rollCubes.forEach((i) => roll(i));
-  checkResults();
+  const randomSortedCubes = Array.from(cubesContainer.children).sort((a, b) => (Math.random() > 0.5 ? 1 : -1)).map(i => i.querySelector(".cube"));
+  const rollResults = randomSortedCubes.map((i) => roll(i));
+  checkResults(randomSortedCubes, rollResults);
 });
 successButton.addEventListener("click", async () => {
   isSuccessfullRoll = true;
